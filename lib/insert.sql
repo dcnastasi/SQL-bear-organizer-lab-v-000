0fb581d761f37b78da8bf3d45a6897337b97e3f9
Dennis Nastasi <dcnastasi@gmail.com>
2019-08-15 17:02:00 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,2 @@
 INSERT INTO bears (id , name, age, gender, color, temperament, alive)
-VALUES (1, "Mr. Chocolate", 1, "male", "black", "happy", true)
+VALUES (1, "Mr. Chocolate", 1, "male", "black", "happy", true),
