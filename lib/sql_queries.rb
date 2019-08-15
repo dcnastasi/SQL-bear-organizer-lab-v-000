da3949d41fe802a195ae53f407d7cb57d78dbff3
Dennis Nastasi <dcnastasi@gmail.com>
2019-08-15 17:19:21 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,15 +20,23 @@ end
 def selects_oldest_bear_and_returns_name_and_age
   "SELECT name, age
   FROM bears
-  WHERE "
+  WHERE name IS NOT NULL
+  ORDER BY age desc 
+  LIMIT 1;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "SELECT name, age
+  FROM bears
+  WHERE name IS NOT NULL
+  ORDER BY age ASC 
+  LIMIT 1;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  "SELECT color, COUNT(*)
+  FROM bears
+  GROUP BY color"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
