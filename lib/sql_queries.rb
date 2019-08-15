d601d82f7ec572eb6d038677c34e7432ea5edd5e
Dennis Nastasi <dcnastasi@gmail.com>
2019-08-15 17:19:44 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -36,7 +36,9 @@ end
 def selects_most_prominent_color_and_returns_with_count
   "SELECT color, COUNT(*)
   FROM bears
-  GROUP BY color"
+  GROUP BY color
+  ORDER BY COUNT(*)
+  LIMIT 1;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
