6b684a55493465a06b9fcd29c8bca76206350621
Dennis Nastasi <dcnastasi@gmail.com>
2019-08-15 17:11:44 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -18,9 +18,7 @@ def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT name, age 
-  FROM bears 
-  "
+  ""
 end
 
 def select_youngest_bear_and_returns_name_and_age
@@ -32,9 +30,13 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "SELECT COUNT(*)
+  FROM bears
+  WHERE temperament = 'goofy';"
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  "SELECT *
+  FROM bears 
+  WHERE name IS NULL;"
 end
